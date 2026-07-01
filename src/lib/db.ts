cf1fae163c79c3ea44fa7abb336807dbf079701a
Z User <z@container>
2026-07-01 10:30:22 +0000

    fix: correct PrismaLibSql casing (lowercase 'ql')

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
@@ -16,7 +16,7 @@ function createPrismaClient(): PrismaClient {
       authToken: process.env.DATABASE_AUTH_TOKEN || undefined,
     })
 
-    const adapter = new PrismaLibSQL(libsql)
+    const adapter = new PrismaLibSql(libsql)
 
     return new PrismaClient({
       adapter,
